05b7075533ea0256196bbaa867d88c8a8a4807b5
shiftUp Digital | Elio Mondlane <liomondlane1@gmail.com>
2026-07-03 11:28:01 +0200

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkMode()
+})
+
+
+
+const year = document.getElementById("year")
+const thisYear = new Date().getFullYear()
+year.setAttribute("datetime", thisYear)
+year.textContent = thisYear
